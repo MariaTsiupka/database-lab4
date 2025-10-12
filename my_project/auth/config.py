7fc104a72170a260302f80b656e5de3169968ac4
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-12 23:10:22 +0300

    Update config.py

--- my_project/auth/config.py
+++ my_project/auth/config.py
@@ -2,12 +2,11 @@ import os
 import sys
 
 os.environ['LANG'] = 'en_US.UTF-8'
-
 sys.dont_write_bytecode = True
 
 class Config:
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_PORT = os.environ.get('MYSQL_PORT', 1401)
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'rozetka333_db')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'ab1.mysql.database.azure.com')
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'maria')           
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')       
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'lab_db')             
