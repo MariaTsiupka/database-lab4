6f2ad8bb1063b0c41a48780427c286e5a3ef1356
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-12 23:20:36 +0300

    Update config.py

--- my_project/auth/config.py
+++ my_project/auth/config.py
@@ -8,5 +8,5 @@ class Config:
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'ab1.mysql.database.azure.com')
     MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
     MYSQL_USER = os.environ.get('MYSQL_USER', 'maria')           
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')       
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '42ohosisSD')    
     MYSQL_DB = os.environ.get('MYSQL_DB', 'lab_db')             
