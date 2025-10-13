106f39c3bcf54e641e46c6c324a0eb4d061801e9
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-13 14:01:23 +0300

    Update review_controller.py

--- my_project/auth/controller/review_controller.py
+++ my_project/auth/controller/review_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from services.review_service import ReviewService
+from ..services.review_service import ReviewService
 
 def create_review_controller(mysql):
     review_controller = Blueprint('review', __name__)
