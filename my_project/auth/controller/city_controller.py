f18668093491464a88573b2eb861ee41c55c6f34
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-13 13:56:57 +0300

    Update city_controller.py

--- my_project/auth/controller/city_controller.py
+++ my_project/auth/controller/city_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from services.city_service import CityService
+from ..services.city_service import CityService
 
 def create_city_controller(mysql):
     city_controller = Blueprint('city', __name__)
