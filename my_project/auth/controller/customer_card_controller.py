30b50c1888add1f48a60ac81ef1fc23271ab0334
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-28 00:14:38 +0200

    Fix import path for CustomerCardService

--- my_project/auth/controller/customer_card_controller.py
+++ my_project/auth/controller/customer_card_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flasgger import swag_from
-from ..services.customer_card_service import CustomerCardService
+from my_project.auth.services.customer_card_service import CustomerCardService
 
 def create_customer_card_controller(mysql):
     customer_card_controller = Blueprint('customer_card', __name__)
