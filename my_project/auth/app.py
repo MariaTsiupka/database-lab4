8d9fddfcf14c0754b64844572be681a0cba26d06
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-28 00:02:58 +0200

    Refactor imports and add index route
    Updated import paths for controllers and added a new index route.

--- my_project/auth/app.py
+++ my_project/auth/app.py
@@ -1,70 +1,30 @@
 from flask import Flask
 from flask_mysqldb import MySQL
-from .config import Config
+from my_project.auth.config import Config
 from flasgger import Swagger
-from .controller.customer_controller import create_customer_controller  
-from .controller.customer_card_controller import create_customer_card_controller 
-from .controller.product_controller import create_product_controller 
-from .controller.company_controller import create_company_controller
-from .controller.order_controller import create_order_controller
-from .controller.order_item_controller import create_order_item_controller
-from .controller.city_controller import create_city_controller
-from .controller.delivery_controller import create_delivery_controller 
-from .controller.payment_controller import create_payment_controller 
-from .controller.review_controller import create_review_controller 
-from .controller.wishlist_controller import create_wishlist_controller 
-from .controller.product_type_controller import create_product_type_controller 
-from .controller.product_attribute_controller import create_product_attribute_controller 
-from .controller.order_feedback_controller import create_order_feedback_controller
+from my_project.auth.controller.customer_controller import create_customer_controller  
+from my_project.auth.controller.customer_card_controller import create_customer_card_controller 
+from my_project.auth.controller.product_controller import create_product_controller 
+from my_project.auth.controller.company_controller import create_company_controller
+from my_project.auth.controller.order_controller import create_order_controller
+from my_project.auth.controller.order_item_controller import create_order_item_controller
+from my_project.auth.controller.city_controller import create_city_controller
+from my_project.auth.controller.delivery_controller import create_delivery_controller 
+from my_project.auth.controller.payment_controller import create_payment_controller 
+from my_project.auth.controller.review_controller import create_review_controller 
+from my_project.auth.controller.wishlist_controller import create_wishlist_controller
+from my_project.auth.controller.product_type_controller import create_product_type_controller
+from my_project.auth.controller.product_attribute_controller import create_product_attribute_controller
+from my_project.auth.controller.order_feedback_controller import create_order_feedback_controller
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mysql = MySQL(app)
 swagger = Swagger(app)
 
-customer_controller = create_customer_controller(mysql)
-app.register_blueprint(customer_controller)
-
-customer_card_controller = create_customer_card_controller(mysql)
-app.register_blueprint(customer_card_controller)
-
-product_controller = create_product_controller(mysql)
-app.register_blueprint(product_controller)
-
-company_controller = create_company_controller(mysql)
-app.register_blueprint(company_controller)
-
-order_controller = create_order_controller(mysql)
-app.register_blueprint(order_controller) 
-
-order_item_controller = create_order_item_controller(mysql)
-app.register_blueprint(order_item_controller)
-
-city_controller = create_city_controller(mysql)
-app.register_blueprint(city_controller) 
-
-delivery_controller = create_delivery_controller(mysql)
-app.register_blueprint(delivery_controller) 
-
-payment_controller = create_payment_controller(mysql)
-app.register_blueprint(payment_controller) 
-
-review_controller = create_review_controller(mysql)
-app.register_blueprint(review_controller) 
-
-wishlist_controller = create_wishlist_controller(mysql)
-app.register_blueprint(wishlist_controller)
-
-product_type_controller = create_product_type_controller(mysql)
-app.register_blueprint(product_type_controller)
-
-
-product_attribute_controller = create_product_attribute_controller(mysql)
-app.register_blueprint(product_attribute_controller)
-
-order_feedback_controller = create_order_feedback_controller(mysql)
-app.register_blueprint(order_feedback_controller)
-
+@app.route('/')
+def index():
+    return "API працює!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
