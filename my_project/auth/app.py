edc9e09239eeac81ac45781308ac9c6020d5486f
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-27 20:24:41 +0200

    Update app.py

--- my_project/auth/app.py
+++ my_project/auth/app.py
@@ -66,6 +66,5 @@ order_feedback_controller = create_order_feedback_controller(mysql)
 app.register_blueprint(order_feedback_controller)
 
 
-if name == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
