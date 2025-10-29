2ede372fddf5c7601724163882ba2c05a66ab5bb
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-29 11:35:18 +0200

    Update app.py

--- my_project/auth/app.py
+++ my_project/auth/app.py
@@ -1,31 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_mysqldb import MySQL
 from my_project.auth.config import Config
 from flasgger import Swagger
 
 from my_project.auth.controller.customer_controller import create_customer_controller  
 from my_project.auth.controller.customer_card_controller import create_customer_card_controller 
-from my_project.auth.controller.product_controller import create_product_controller
+from my_project.auth.controller.product_controller import create_product_controller 
 from my_project.auth.controller.company_controller import create_company_controller
 from my_project.auth.controller.order_controller import create_order_controller
 from my_project.auth.controller.order_item_controller import create_order_item_controller
 from my_project.auth.controller.city_controller import create_city_controller
 from my_project.auth.controller.delivery_controller import create_delivery_controller 
 from my_project.auth.controller.payment_controller import create_payment_controller 
-from my_project.auth.controller.review_controller import create_review_controller
+from my_project.auth.controller.review_controller import create_review_controller 
 from my_project.auth.controller.wishlist_controller import create_wishlist_controller
 from my_project.auth.controller.product_type_controller import create_product_type_controller
 from my_project.auth.controller.product_attribute_controller import create_product_attribute_controller
 from my_project.auth.controller.order_feedback_controller import create_order_feedback_controller
 
-import os  # додано для отримання порту з середовища
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mysql = MySQL(app)
 swagger = Swagger(app)
 
-# реєстрація потрібних Blueprints
+# Реєстрація Blueprints
 app.register_blueprint(create_customer_controller(mysql))
 app.register_blueprint(create_customer_card_controller(mysql))
 app.register_blueprint(create_product_controller(mysql))
@@ -41,7 +41,7 @@ app.register_blueprint(create_product_type_controller(mysql))
 app.register_blueprint(create_product_attribute_controller(mysql))
 app.register_blueprint(create_order_feedback_controller(mysql))
 
-# Додано для перевірки всіх маршрутів
+# Для перевірки всіх маршрутів
 print("Доступні маршрути:")
 print(app.url_map)
 
@@ -49,7 +49,11 @@ print(app.url_map)
 def index():
     return "API працює!"
 
+# Тестовий endpoint для Azure
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"}), 200
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # береться порт з середовища Azure
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
- 
