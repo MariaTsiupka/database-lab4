a59fcdffd53bf130a8e545a79b61c9dacc3aefa9
MariaTsiupka <151679439+MariaTsiupka@users.noreply.github.com>
2025-10-29 11:25:36 +0200

    Update app.py

--- my_project/auth/app.py
+++ my_project/auth/app.py
@@ -5,25 +5,27 @@ from flasgger import Swagger
 
 from my_project.auth.controller.customer_controller import create_customer_controller  
 from my_project.auth.controller.customer_card_controller import create_customer_card_controller 
-from my_project.auth.controller.product_controller import create_product_controller 
+from my_project.auth.controller.product_controller import create_product_controller
 from my_project.auth.controller.company_controller import create_company_controller
 from my_project.auth.controller.order_controller import create_order_controller
 from my_project.auth.controller.order_item_controller import create_order_item_controller
 from my_project.auth.controller.city_controller import create_city_controller
 from my_project.auth.controller.delivery_controller import create_delivery_controller 
 from my_project.auth.controller.payment_controller import create_payment_controller 
-from my_project.auth.controller.review_controller import create_review_controller 
+from my_project.auth.controller.review_controller import create_review_controller
 from my_project.auth.controller.wishlist_controller import create_wishlist_controller
 from my_project.auth.controller.product_type_controller import create_product_type_controller
 from my_project.auth.controller.product_attribute_controller import create_product_attribute_controller
 from my_project.auth.controller.order_feedback_controller import create_order_feedback_controller
+
+import os  # додано для отримання порту з середовища
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mysql = MySQL(app)
 swagger = Swagger(app)
 
-# реєстрація тільки потрібних Blueprints
+# реєстрація потрібних Blueprints
 app.register_blueprint(create_customer_controller(mysql))
 app.register_blueprint(create_customer_card_controller(mysql))
 app.register_blueprint(create_product_controller(mysql))
@@ -48,4 +50,6 @@ def index():
     return "API працює!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # береться порт з середовища Azure
+    app.run(host='0.0.0.0', port=port, debug=True)
+ 
